03f26ef5c80ea82c1f05fc54b276822ce0e1f71d
BluuWasTakenAlt <>
2021-08-08 20:39:13 +0100

    Command Line Parsing

--- WorldsAdriftReborn/Program.cs
+++ WorldsAdriftReborn/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using WorldsAdriftReborn.Patching;
@@ -8,11 +9,40 @@ namespace WorldsAdriftReborn
 {
     internal class Program
     {
+        internal static bool ParseArgs(string[] args, ref string GameInstallationDirectory) 
+        {
+            GameInstallationDirectory = "";
+            for (int i = 0; i < args.Length; i++)
+            {
+                string arg = args[i];
+
+                switch(arg)
+                {
+                    case "--dir":
+                        GameInstallationDirectory = args[i + 1];
+                        break;
+                }
+            }
+
+            //Default
+            if (GameInstallationDirectory == "")
+            {
+                GameInstallationDirectory = Directory.GetCurrentDirectory();
+            }
+
+            return true;
+        }
+
         public static void Main(string[] args)
         {
-            IPatcher patcher = new DnlibPatcher();
+            string GameInstallationDirectory = "";
+            ParseArgs(args, ref GameInstallationDirectory);
 
+            IPatcher patcher = new DnlibPatcher();
             patcher.PatchAll();
+
+            //Make sure console doesn't just close right away
+            Console.ReadLine();
         }
     }
 }
