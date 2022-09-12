4fb933b5513ddded523fc4ec6d45483199695c0a
sp00ktober <ezra.knobloch@protonmail.com>
2022-09-13 01:02:22 +0200

    fixed most errors popping up when instantiating the player (except discord rich presence). now player is constantly falling into the sky (loading screen removed manually)

---
+++ WorldsAdriftReborn/Patching/BypassPlayerAnalytics/PlayerAnalytics_Patch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Bossa.Travellers.Analytics;
+using HarmonyLib;
+
+namespace WorldsAdriftReborn.Patching.BypassPlayerAnalytics
+{
+    [HarmonyPatch(typeof(PlayerAnalytics))]
+    internal class PlayerAnalytics_Patch
+    {
+        // if needed we can enable this again later but for now i disable it because it has a few nullrefs that i dont want to fix rn
+        // important for FreezeDetector.Update()
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(PlayerAnalytics.GeneratePerformanceReport))]
+        public static bool GeneratePerformanceReport(ref Dictionary<string, object> __result)
+        {
+            __result = new Dictionary<string, object>();
+            return false;
+        }
+    }
+}
--- WorldsAdriftReborn/Patching/LoadInGame/PrepareForGameState_Patch.cs
+++ WorldsAdriftReborn/Patching/LoadInGame/PrepareForGameState_Patch.cs
@@ -4,8 +4,14 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
 using Assets.Scripts.Physics;
+using Assets.Scripts.Player;
+using Bossa.Travellers.Controls;
+using Bossa.Travellers.Creatures;
+using Bossa.Travellers.Items;
 using GameStateMachine;
 using HarmonyLib;
+using Improbable;
+using Improbable.Worker;
 using UnityEngine;
 
 namespace WorldsAdriftReborn.Patching.LoadInGame
@@ -46,6 +52,49 @@ namespace WorldsAdriftReborn.Patching.LoadInGame
                                 {
                                     Debug.Log("FOUND INIT");
                                     LocalPlayer.Instance.SetLocalPlayer(init);
+
+                                    // the following should be set from server i think
+                                    // but for now i set them here
+
+                                    Bossa.Travellers.Player.HealthStateData hData = new Bossa.Travellers.Player.HealthStateData();
+                                    hData.health = 100;
+                                    hData.maxHealth = 200;
+                                    hData.alive = true;
+                                    hData.invulnerableTime = 0;
+                                    hData.autoRecover = true;
+                                    hData.entitiesToNotifyOfDeath = new Improbable.Collections.List<EntityId>();
+                                    hData.impactDamageMultiplier = 1;
+                                    hData.totalDamageMultiplier = 1;
+
+                                    PilotStateData pData = new PilotStateData();
+                                    pData.drivingEntityId = new Improbable.EntityId(0);
+                                    pData.controlEntityId = new Improbable.EntityId(0);
+                                    pData.controlType = ControlVehicleType.None;
+
+                                    ShipHullAgentStateData sData = new ShipHullAgentStateData();
+                                    sData.schematics = new Improbable.Collections.List<ShipHullSchematicData>();
+                                    sData.editorId = new EntityId(0);
+
+                                    Bossa.Travellers.Player.HealthState.Impl hImpl = new Bossa.Travellers.Player.HealthState.Impl();
+                                    AccessTools.Field(typeof(Bossa.Travellers.Player.HealthState.Impl), "data").SetValue(hImpl, new Bossa.Travellers.Player.HealthState.Data(hData));
+
+                                    Type tConnectionHandle = AccessTools.TypeByName("ConnectionHandle");
+                                    Type tConnection = AccessTools.TypeByName("Connection");
+                                    PilotState.Impl pImpl = new PilotState.Impl((Connection)AccessTools.Constructor(tConnection, new Type[] { tConnectionHandle }).Invoke(new object[] { AccessTools.Constructor(tConnectionHandle).Invoke(new object[] { }) }), new EntityId(0), new PilotState.Data(pData)); // wtf
+
+                                    ShipHullAgentState.Impl sImpl = new ShipHullAgentState.Impl((Connection)AccessTools.Constructor(tConnection, new Type[] { tConnectionHandle }).Invoke(new object[] { AccessTools.Constructor(tConnectionHandle).Invoke(new object[] { }) }), new EntityId(0), new ShipHullAgentState.Data(sData));
+
+                                    LocalPlayer.Instance.healthVisualiser._health = hImpl;
+
+                                    PlayerExternalData eData = (PlayerExternalData)AccessTools.Field(typeof(PlayerMove), "externalData").GetValue(LocalPlayer.Instance.playerMove);
+
+                                    Type tPlayerExternalDataVisualizer = AccessTools.TypeByName("PlayerExternalDataVisualizer");
+                                    AccessTools.Field(tPlayerExternalDataVisualizer, "_pilotState").SetValue(eData, pImpl);
+                                    AccessTools.Field(tPlayerExternalDataVisualizer, "_healthState").SetValue(eData, hImpl);
+                                    AccessTools.Field(tPlayerExternalDataVisualizer, "_hullEditState").SetValue(eData, sImpl);
+
+                                    AccessTools.Field(typeof(PlayerMove), "externalData").SetValue(LocalPlayer.Instance.playerMove, eData);
+
                                     Debug.Log("SETTING PLAYER ACTIVE"); // maybe not needed
                                     go2.SetActive(true);
                                     break;
