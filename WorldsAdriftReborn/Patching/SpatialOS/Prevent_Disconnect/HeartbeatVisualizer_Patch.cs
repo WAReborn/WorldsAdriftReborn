7a8170c614f0500ef1385ebc7b9de32b0b5d94c5
sp00ktober <ezra.knobloch@protonmail.com>
2022-09-30 22:19:23 +0200

    add patch to prevent game from disconnecting from our server due to wrong init values supplied

---
+++ WorldsAdriftReborn/Patching/SpatialOS/Prevent_Disconnect/HeartbeatVisualizer_Patch.cs
@@ -0,0 +1,18 @@
+﻿using Bossa.Travellers.Visualisers.HealthCheck;
+using HarmonyLib;
+
+namespace WorldsAdriftReborn.Patching.SpatialOS.Prevent_Disconnect
+{
+    [HarmonyPatch(typeof(HeartbeatVisualiser))]
+    internal class HeartbeatVisualizer_Patch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(HeartbeatVisualiser), "Update")]
+        public static bool Update_Prefix()
+        {
+            // the only purpose of the original method is to check for Time.realtimeSinceStartup - this.lastGsimMessage > 65f and if thats the case disconnect from server
+            // as we have a lot of temp and dummy data atm we just skip this method, but should fix it up later on
+            return false;
+        }
+    }
+}
