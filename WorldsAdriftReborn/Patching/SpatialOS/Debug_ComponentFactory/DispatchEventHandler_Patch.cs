d61034b9ab869855f5a04a9cfab7f91da1fd095f
sp00ktober <ezra.knobloch@protonmail.com>
2023-02-02 18:47:35 +0100

    add debug patch to partially fix ToolBehavior

--- WorldsAdriftReborn/Patching/SpatialOS/Debug_ComponentFactory/DispatchEventHandler_Patch.cs
+++ WorldsAdriftReborn/Patching/SpatialOS/Debug_ComponentFactory/DispatchEventHandler_Patch.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
+using Bossa.Travellers.Player;
 using HarmonyLib;
+using Improbable;
 using Improbable.Entity.Component;
+using Improbable.Unity.Internal;
+using Improbable.Unity.Visualizer;
+using Improbable.Util.Injection;
 using Newtonsoft.Json.Serialization;
 using UnityEngine;
 
@@ -38,6 +43,36 @@ namespace WorldsAdriftReborn.Patching.SpatialOS.Debug_ComponentFactory
                     }),
                     new CodeInstruction(OpCodes.Pop))
                 .InstructionEnumeration();
+        }
+    }
+
+    // this is just a test to manually set required fields in ToolBeaviour when they are needed to pass a check to get it enabled
+    [HarmonyPatch(typeof(EntityVisualizers))]
+    internal class EntityVisualizers_Patch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(EntityVisualizers), "UpdateActivation")]
+        public static void UpdateActivation_Prefix(EntityVisualizers __instance, MonoBehaviour visualizer )
+        {
+            if(visualizer.GetType() == typeof(ToolBehaviour))
+            {
+                // reflection hell and very broken code incomming but it might make the inventory work
+                // however this is brute force and we should find out how the game usually does this
+                System.Type tConnectionHandle = AccessTools.TypeByName("ConnectionHandle");
+                System.Type tConnection = AccessTools.TypeByName("Connection");
+
+                ToolState.Impl impl1 = new ToolState.Impl((Improbable.Worker.Connection)AccessTools.Constructor(tConnection, new System.Type[] { tConnectionHandle }).Invoke(new object[] { AccessTools.Constructor(tConnectionHandle).Invoke(new object[] { }) }), new EntityId(0), new ToolState.Data(3));
+                ToolRequestState.Impl impl2 = new ToolRequestState.Impl((Improbable.Worker.Connection)AccessTools.Constructor(tConnection, new System.Type[] { tConnectionHandle }).Invoke(new object[] { AccessTools.Constructor(tConnectionHandle).Invoke(new object[] { }) }), new EntityId(0), new ToolRequestState.Data());
+
+                IMemberAdapter impl1FieldInfo = VisualizerMetadataLookup.Instance.GetFieldInfo(impl1.GetType(), visualizer.GetType());
+                IMemberAdapter impl2FieldInfo = VisualizerMetadataLookup.Instance.GetFieldInfo(impl2.GetType(), visualizer.GetType());
+
+                if(impl1FieldInfo != null && impl2FieldInfo != null)
+                {
+                    AccessTools.Method(typeof(EntityVisualizers), "InjectField").Invoke(__instance, new object[] { visualizer, impl1FieldInfo, impl1});
+                    AccessTools.Method(typeof(EntityVisualizers), "InjectField").Invoke(__instance, new object[] { visualizer, impl2FieldInfo, impl2 });
+                }
+            }
         }
     }
 }
