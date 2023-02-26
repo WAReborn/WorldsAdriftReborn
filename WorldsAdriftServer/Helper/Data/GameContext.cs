e1c47092ddc47c3e6b594a304d0c9a4f54cbefaf
Geoffrey Everaert <geoff.everaert@gmail.com>
2023-02-26 18:30:55 +0100

    Save characters in database, fix create character

---
+++ WorldsAdriftServer/Helper/Data/GameContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WorldsAdriftServer.Objects.CharacterSelection;
+
+namespace WorldsAdriftServer.Helper.Data
+{
+    public class GameContext : DbContext
+    {
+        public DbSet<CharacterDataDTO> Characters { get; set; }
+
+        public string DbPath { get; }
+
+        public GameContext()
+        {
+            DbPath = Path.Join(Directory.GetCurrentDirectory(), "WAR.db");
+        }
+
+        protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder ) 
+            => optionsBuilder.UseSqlite($"Data Source={DbPath}");
+
+    }
+}
